5372047cfab994ff76f31074860dbd27311a6345
Dexterlente <dexter.lente18@gmail.com>
2022-11-07 04:47:44 +0800

    done part design phase

--- final/finalproject/static/finalproject/price.js
+++ final/finalproject/static/finalproject/price.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Use buttons to toggle between views
     document.querySelector('#crypto').addEventListener('click', () => load_crypto);
-   // document.querySelector('#token').addEventListener('click', () => load_token);
+    document.querySelector('#token').addEventListener('click', () => load_token);
 
   
     // By default, load the crypto
@@ -16,38 +16,64 @@ function load_crypto() {
     document.querySelector('#cryto-content').style.display = 'block';
 
 
-    const crypto_api = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Ccardano%2Csolana%2Cdogecoin%2Cpolkadot%2Cmatic-network%2Cavalanche-2%2Cuniswap%2Ctron%2Clitecoin&vs_currencies=php%2Cusd&include_24hr_change=true'
-    
+    //const crypto_api = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Ccardano%2Csolana%2Cdogecoin%2Cpolkadot%2Cmatic-network%2Cavalanche-2%2Cuniswap%2Ctron%2Clitecoin&vs_currencies=php%2Cusd&include_24hr_change=true'
+    const crypto_api ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Ccardano%2Csolana%2Cdogecoin%2Cpolkadot%2Cmatic-network%2Cavalanche-2%2Cuniswap%2Ctron%2Clitecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     fetch(crypto_api)
     .then((response) => response.json())
     .then(data => {
-          let object = Object.entries(data)
-          object.forEach(new_data => {
+          data.forEach(element => {
+            console.log(element)
+
             
-          let last_object = Object.values(new_data)
-          last_object.forEach(last_data =>{
 
-              console.log(last_data)
+            const price_php = element.current_price.toLocaleString("en-US");
+            const marketcap = element.market_cap.toLocaleString("en-US");
+            const percent = parseFloat(element.market_cap_change_percentage_24h).toFixed(2);
 
-              const makeMessage = document.createElement('div')
-              makeMessage.className = "cryptopotatoes"
-              makeMessage.innerHTML= `
-              ${last_data.php} ${last_data.usd} ${last_data.usd_24h_change}
-              `
-  //must print only the the value
-             document.querySelector('#crypto-view').append(makeMessage);
+            const cryptoes = document.createElement('div');
+            cryptoes.className = 'd-flex justify-content-center p-1'
+            cryptoes.innerHTML = `
+            <div class="col-1 mr-1"><img src="${element.image}" width="35" height="35"></div>  <div class="col-2">${element.name}</div><div class="col-2 mx-2">₱${price_php}</div> <div class="col-2 ml-2">${percent}%</div> <div class="col-auto pl-1">₱${marketcap}</div>
+            `;
 
-            })
+            if (percent >= 0){
+              cryptoes.style.color = "green"
+            }
+            else {
+              cryptoes.style.color = "red"
+            }
             
-          })
-    }
-    );
+
+            document.querySelector("#cryto-view").append(cryptoes);
+          });
+        })
+      }
+          // let object = Object.entries(data)
+          // console.log(object[0])
+          // object.forEach(new_data => {
+
+          //   let final = Object.values(new_data)
+          //   console.log(final)
+
+
+  //             const makeMessage = document.createElement('div')
+  //             makeMessage.className = "d-flex justify-content-center"
+  //             makeMessage.innerHTML= `
+  //             ${final[0]} ${final[2]} ${final[3]} 
+  //             `
+  // //must print only the the value
+             
+
+
+            
+
+   // );
   
         // new_data(data));
         // console.log(new_data);
 
     //document.querySelector('#crypto-content').innerHTML = `${data.bitcoin.php}`
-      }       
+     // }       
     //   data.crypto_api.forEach((new_data) => {
     //     console.log(data)
     //     console.log(new_data)
@@ -68,3 +94,46 @@ function load_crypto() {
 //   console.table(data.tron)
 //   console.table(data.litecoin)
 // }
+
+
+
+
+function load_token() {
+  document.querySelector('#token-content').style.display = 'block';
+  document.querySelector('#cryto-content').style.display = 'none';
+
+
+  //const crypto_api = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Ccardano%2Csolana%2Cdogecoin%2Cpolkadot%2Cmatic-network%2Cavalanche-2%2Cuniswap%2Ctron%2Clitecoin&vs_currencies=php%2Cusd&include_24hr_change=true'
+  // const crypto_api ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Ccardano%2Csolana%2Cdogecoin%2Cpolkadot%2Cmatic-network%2Cavalanche-2%2Cuniswap%2Ctron%2Clitecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  const token_api =''
+  
+  fetch(token_api)
+  .then((response) => response.json())
+  .then(data => {
+        data.forEach(element => {
+          console.log(element)
+
+          
+
+          // const price_php = element.current_price.toLocaleString("en-US");
+          // const marketcap = element.market_cap.toLocaleString("en-US");
+          // const percent = parseFloat(element.market_cap_change_percentage_24h).toFixed(2);
+
+          // const cryptoes = document.createElement('div');
+          // cryptoes.className = 'd-flex justify-content-center p-1'
+          // cryptoes.innerHTML = `
+          // <div class="col-1 mr-1"><img src="${element.image}" width="35" height="35"></div>  <div class="col-2">${element.name}</div><div class="col-2 mx-2">₱${price_php}</div> <div class="col-2 ml-2">${percent}%</div> <div class="col-auto pl-1">₱${marketcap}</div>
+          // `;
+
+          // if (percent >= 0){
+          //   cryptoes.style.color = "green"
+          // }
+          // else {
+          //   cryptoes.style.color = "red"
+          // }
+          
+
+          document.querySelector("#token-view").append(cryptoes);
+        });
+      })
+}
